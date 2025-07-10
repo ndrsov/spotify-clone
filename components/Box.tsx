535637285dd948e1d12ce8bb30997ba5adbb9d94
Andres Soria <andres.zov@gmail.com>
2025-07-10 12:45:42 -0600

    Refactor Box component to apply tailwind styles for improved layout

--- components/Box.tsx
+++ components/Box.tsx
@@ -1,10 +1,18 @@
+import { twMerge } from 'tailwind-merge';
+
 interface BoxProps {
   children: React.ReactNode;
   className?: string;
 }
 
 const Box: React.FC<BoxProps> = ({ children, className }) => {
-  return <div>{children}</div>;
+  return (
+    <div
+      className={twMerge('bg-neutral-900 rounded-leg h-fit w-full', className)}
+    >
+      {children}
+    </div>
+  );
 };
 
 export default Box;
