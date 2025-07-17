86323eb5314332dfb35bb4c802bf6499ea92840c
Andres Soria <andres.zov@gmail.com>
2025-07-17 11:40:23 -0600

    Update background color of Home component

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="bg-neutral-500 rounded-lg h-full w-full overflow-hidden overflow-y-auto">
+    <div className="bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto">
       Spotify clone
     </div>
   );
